732d86df3a0d2b1ec90453abe0997fd961b77a7b
reddynie11 <reddy.nie11@gmail.com>
2019-11-02 18:14:59 +0530

    moving state to main component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
-import './App.css';
+import ;
 
 import NavBar from './components/Navbar/Navbar';
 import UserList from './components/users/UserList'
 
 class App extends React.Component {
+  state={
+    users:[],
+    loading: false
+  }
+
+  async componentDidMount(){
+    this.setState({ loading: true });
+    const fetchUsers = axios.get('https://api.github.com/users');
+    this.setState({ users: fetchUsers, loading : false })
+  }
+
  render(){
   return (
     <div className="">
       <NavBar/>
-      <div className='container'> <UserList /></div>
+      <div className='container'> <UserList users={this.state.users}/></div>
     </div>
   );
  }
